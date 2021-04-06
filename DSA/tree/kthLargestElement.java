fd8be69eccde872ee5660975f333dd1fa0460d20
Arindam <arindamkeswani@gmail.com>
2021-04-06 23:45:04 +0530

    Updated DSA-generic tree codes

---
+++ DSA/tree/kthLargestElement.java
@@ -0,0 +1,102 @@
+// 1. You are given a partially written GenericTree class.
+// 2. You are given a number k. You are required to find and print the kth largest value in the tree.
+// 3. Input and Output is managed for you.
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  
+  static int ceil;
+  static int floor;
+  public static void ceilAndFloor(Node node, int data) {
+    if(node.data > data){
+      if(node.data < ceil){
+        ceil = node.data;
+      }
+    }
+
+    if(node.data < data){
+      if(node.data > floor){
+        floor = node.data;
+      }
+    }
+
+    for (Node child : node.children) {
+      ceilAndFloor(child, data);
+    }
+  }
+
+  public static int kthLargest(Node node, int k){
+    // write your code here
+    
+    int kth=Integer.MAX_VALUE; //declate answer as infinity
+    
+    for(int i=0;i<k;i++){
+        floor=Integer.MIN_VALUE; 
+        ceilAndFloor(node,kth); //every time this function is called, it will return the next largest value
+        kth=floor; //we store this next largest value in floor
+    }
+    
+    return kth;
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    int k = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    int kthLargest = kthLargest(root, k);
+    System.out.println(kthLargest);
+  }
+
+}
