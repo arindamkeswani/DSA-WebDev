a9e78f27a73869be1b13c83f5ccf2bd6726e976e
Arindam <arindamkeswani@gmail.com>
2021-04-17 15:34:41 +0530

    Formatted PDF O/P via console input

--- Hackathon/Hackathon1/jarvis.js
+++ Hackathon/Hackathon1/jarvis.js
@@ -10,27 +10,16 @@ let path=require('path');
 let PDFDocument= require('pdfkit');
 
 var summarize = require("text-summary");
+var os = require("os");
 
 (async function () {
     try {
         let browserInstance = await puppeteer.launch({
-            headless: false,
+            headless: true,
             defaultViewport: null,
             args: ["--start-maximized"]
         });
-        let fullSum = await getLinksFromGoogle(url, browserInstance, query);
-
-        // await console.log(fullSum);
-        // await getSum(fullSum[0],browserInstance);
-        // await getSum(fullSum[1],browserInstance);
-        // await getSum(fullSum[2],browserInstance);
-        // await getSum(fullSum[3],browserInstance);
-        // await getSum(fullSum[4],browserInstance);
-        // await getSum(fullSum[5],browserInstance);
-        // await getSum(fullSum[6],browserInstance);
-        // await getSum(fullSum[7],browserInstance);
-        // await getSum(fullSum[8],browserInstance);
-        // await getSum(fullSum[9],browserInstance);
+        await getLinksFromGoogle(url, browserInstance, query, os);
 
 
     } catch (err) {
@@ -40,7 +29,7 @@ var summarize = require("text-summary");
 
 
 
-async function getLinksFromGoogle(link, browserInstance, query) {
+async function getLinksFromGoogle(link, browserInstance, query, os) {
     let newTab = await browserInstance.newPage();
     await newTab.setDefaultNavigationTimeout(0); 
     await newTab.goto(link);
@@ -58,14 +47,15 @@ async function getLinksFromGoogle(link, browserInstance, query) {
         }
         else{
             // console.log(html);
-            await printTopArtName(html,browserInstance,query);
+            await printTopArtName(html,browserInstance,query, os);
         }
     }
 }
 
 
 
-async function printTopArtName(html,browserInstance,query){
+async function printTopArtName(html,browserInstance,query, os){
+    var os = require("os");
     let selectorTool= cheerio.load(html);
     let newsArray=  selectorTool(".ipQwMb.ekueJc.RD0gLb a");
     // console.log(newsArray);
@@ -82,19 +72,19 @@ async function printTopArtName(html,browserInstance,query){
         linkArray.push({fulllink,heading})
         // console.log(fulllink);
     }
-    
+    await console.log("Top 5 links retrieved");
     // console.log(linkArray[0]["heading"]); //retieve heading at index 0
     let ans= "";
     for(let i=0;i<linkArray.length;i++){
         // let ans_temp = await getSum(linkArray[i],browserInstance);
         // ans+=await ans+ ans_temp;
         ans= ans+ (i+1);
-        ans= ans+ "\n\nTitle: " + linkArray[i]["heading"];
-        ans= ans+ "\n\nSource: " + linkArray[i]["fulllink"];
-        var singleContent = await getSum(linkArray[i]["fulllink"], browserInstance);
+        ans= ans+"\n\nTitle: " + linkArray[i]["heading"];
+        ans= ans+"\n\nSource: " + linkArray[i]["fulllink"];
+        var singleContent = await getSum(linkArray[i]["fulllink"], browserInstance,os);
         ans+="\n\n"+singleContent;
 
-        console.log("Article",i+1,"fetchecd");
+        console.log("Article",i+1,"fetched");
     }
    
     await console.log("Completed");
@@ -115,63 +105,70 @@ async function printTopArtName(html,browserInstance,query){
 //     }
 // }
 
-async function getSum(url,browserInstance){
+async function getSum(url,browserInstance,os){
     // let selTool = cheerio.load(html);
     // let artContent= selTool("<p>")
     // let artText = selTool(artContent).text();
     // console.log(artContent);
     // // console.log(artText);
-    var summarize = await require("text-summary");
-    let newP = await browserInstance.newPage();
-    await newP.setDefaultNavigationTimeout(0); 
-    await newP.goto(url);
-    // await newP.waitForNavigation({waitUntil:"domcontentloaded"});
-    await newP.waitForSelector("p", 0);
-    // await Promise.all([
-    //     newP.waitForNavigation({waitUntil:"domcontentloaded"}), // The promise resolves after navigation has finished
-    //     newP.goto(url), // Clicking the link will indirectly cause a navigation
-    //   ]);
-    
-    
-    // await newP.waitFor(15000);
-    
-    async function consoleFn(sel) {
-        let t = await document.querySelectorAll(sel);
+    try{
+        var summarize = await require("text-summary");
+        let newP = await browserInstance.newPage();
+        await newP.setDefaultNavigationTimeout(0); 
+        await newP.goto(url);
+        // await newP.waitForNavigation({waitUntil:"domcontentloaded"});
+        await newP.waitForSelector("p", 0);
+        // await Promise.all([
+        //     newP.waitForNavigation({waitUntil:"domcontentloaded"}), // The promise resolves after navigation has finished
+        //     newP.goto(url), // Clicking the link will indirectly cause a navigation
+        //   ]);
         
-        // let PName = document.querySelectorAll(pNameSelector);
-        let details=""
-        // let details = [];
-        if(t!=undefined){
-            for (let i = 0; i < t.length; i++) {
-                let text = await t[i].innerText;
-                
-               details = await details + text+"\n\n";
+        
+        // await newP.waitFor(15000);
+        
+        async function consoleFn(sel,os) {
+            // var os = require("os");
+            let t = await document.querySelectorAll(sel);
+            
+            // let PName = document.querySelectorAll(pNameSelector);
+            let details=""
+            // let details = [];
+            if(t!=undefined){
+                for (let i = 0; i < t.length; i++) {
+                    let text = await t[i].innerText;
+                    
+                details = await details + text+ os.EOL;
+                }
+                return details;
             }
-            return details;
+            else{
+                return "Cannot to retrieve data from",url,"\n\n_____________________________________________________\n\n";
+            }
+            
+            
         }
-        else{
-            return "Unable to retrieve data from",url;
-        }
-        
-        
+
+        // let artText = newP.evaluate(consoleFn,"p")
+        // console.log(artText);
+        let artText =await newP.evaluate(consoleFn,"p",os)
+        var numberSentences = await 10;
+        var summary = await summarize.summary(artText, numberSentences);
+
+        // var Res=await summary.split(".");
+        // var s=""
+        // for(let i=0;i<Res.length;i++){
+        //     s+=Res[i]+"\n\n";
+        // }
+
+        summary=await summary + "\n\n_____________________________________________________\n\n";
+        // await console.log(summary);
+        return await summary;
+
+    }catch(err){
+        console.log(err);
+        return "Unable to retrieve data, but here's the link!--> "+ url+"\n\n_____________________________________________________\n\n";
     }
-
-    // let artText = newP.evaluate(consoleFn,"p")
-    // console.log(artText);
-    let artText =await newP.evaluate(consoleFn,"p")
-    var numberSentences = await 10;
-    var summary = await summarize.summary(artText, numberSentences);
-
-    // var Res=await summary.split(".");
-    // var s=""
-    // for(let i=0;i<Res.length;i++){
-    //     s+=Res[i]+"\n\n";
-    // }
-
-    summary=await summary + "\n_____________________________________________________\n\n"
-    // await console.log(summary);
-    return await summary;
-
+    
     
 }
 
@@ -184,7 +181,7 @@ async function createOP(ans,query){
     let filePath = path.join(__dirname, query+".pdf");
     let pdfDoc = new PDFDocument;
     pdfDoc.pipe(fs.createWriteStream(filePath));
-    pdfDoc.text(JSON.stringify(ans));
+    pdfDoc.text(ans);
     pdfDoc.end();
 
 }
