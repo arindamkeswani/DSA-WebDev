dfdeb814ffdf49d324db9a62d0b8ea29e5330131
Arindam <arindamkeswani@gmail.com>
2021-04-29 22:59:03 +0530

    Added DSA: Practice Test 2 Circle Maniac

--- DSA/PepTests/PracticeTest29-4-21/circleManiacPartial.java
+++ DSA/PepTests/PracticeTest29-4-21/circleManiacPartial.java
@@ -13,40 +13,76 @@
 
 
 
-
 import java.io.*;
 import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner x=new Scanner(System.in);
-        int x1=x.nextInt(); int y1=x.nextInt();
-        int x2=x.nextInt(); int y2=x.nextInt();
-        int r1 = x.nextInt();
-        int r2 = x.nextInt();
+       Scanner scn = new Scanner(System.in);
+        int x1 = scn.nextInt();
+        int y1 = scn.nextInt();
         
-        if(Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) +Math.min(r1,r2) < Math.max(r1,r2) && (x1!=x2 || y1!=y2)){//overlaps
-            System.out.println("overlaps");
-        } 
-        else if(x1==x2 && y1==y2 && r1!=r2){//concentric
+        int x2 = scn.nextInt();
+        int y2 = scn.nextInt();
+        
+        int r1 = scn.nextInt();
+        int r2 = scn.nextInt();
+        
+        //distance between centers        
+        int a = (x1 - x2);        
+        int b = (y1 - y2);
+      
+        double centerDis = Math.sqrt((a*a) + (b*b)) ;
+        
+        //conditions
+        
+        if(centerDis == 0){
             System.out.println("concentric");
-        }
-        else if(Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) > r1+r2){//far-apart
-            System.out.println("far-apart");
-        }
-        else if((Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) == r1+r2) || (Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) == Math.abs(r1-r2))){//touches at 1 point
+        }else if(centerDis == r1+r2){
             System.out.println("touches at 1 point");
-        }
-        else if(Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) < r1+r2){//touches at 2 point
+        }else if(centerDis > r1+r2){
+            System.out.println("far-apart");
+        }else if(centerDis < r1 || centerDis < r2){
+            System.out.println("overlaps");
+        }else if(centerDis < r1 +r2){
             System.out.println("touches at 2 point");
         }
-        
-        
-        
-        
-        
-        
     }
 }
+// import java.io.*;
+// import java.util.*;
+
+// public class Solution {
+
+//     public static void main(String[] args) {
+//         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+//         Scanner x=new Scanner(System.in);
+//         int x1=x.nextInt(); int y1=x.nextInt();
+//         int x2=x.nextInt(); int y2=x.nextInt();
+//         int r1 = x.nextInt();
+//         int r2 = x.nextInt();
+        
+//         if(Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) +Math.min(r1,r2) < Math.max(r1,r2) && (x1!=x2 || y1!=y2)){//overlaps
+//             System.out.println("overlaps");
+//         } 
+//         else if(x1==x2 && y1==y2 && r1!=r2){//concentric
+//             System.out.println("concentric");
+//         }
+//         else if(Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) > r1+r2){//far-apart
+//             System.out.println("far-apart");
+//         }
+//         else if((Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) == r1+r2) || (Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) == Math.abs(r1-r2))){//touches at 1 point
+//             System.out.println("touches at 1 point");
+//         }
+//         else if(Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)) < r1+r2){//touches at 2 point
+//             System.out.println("touches at 2 point");
+//         }
+        
+        
+        
+        
+        
+        
+//     }
+// }
