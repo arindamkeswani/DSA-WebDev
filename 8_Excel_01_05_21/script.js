db0bc71ac80d75291789529f9adcf40ad1f3d605
Arindam <arindamkeswani@gmail.com>
2021-05-03 01:15:31 +0530

    Added upto Font Size, Text Alignment

--- 8_Excel_01_05_21/script.js
+++ 8_Excel_01_05_21/script.js
@@ -1,7 +1,16 @@
 let addbtnContainer = document.querySelector(".add-sheet-container");
 let sheetList= document.querySelector(".sheets-list");
 let firstSheet = document.querySelector(".sheet");
+let Allcells = document.querySelectorAll(".grid .col");
+// console.log(Allcells.classList);
+let addressBar = document.querySelector(".address-box");
+let leftBtn = document.querySelector(".left");
+let rightBtn = document.querySelector(".right");
+let centerBtn = document.querySelector(".center");
+let fontBtn = document.querySelector(".font-size");
+
 firstSheet.addEventListener("click", handleActiveSheet);
+
 
 addbtnContainer.addEventListener("click",function(){
     let sheetsArr = document.querySelectorAll(".sheet");
@@ -27,3 +36,62 @@ function handleActiveSheet(e){
         mySheet.classList.add("active-sheet");
     }
 }
+
+for(let i=0;i<Allcells.length; i++){
+    Allcells[i].addEventListener("click", function handleCell(){
+        let rid=Number(Allcells[i].getAttribute("rid"));
+        let cid=Number(Allcells[i].getAttribute("cid"));
+        let rowAdd = rid+1;
+        let colAdd = String.fromCharCode(cid+65);
+        let address = colAdd + rowAdd;
+        addressBar.value=address;
+    });
+}
+
+leftBtn.addEventListener("click",function(){
+    let address=addressBar.value;
+    
+    let {rid,cid}= getRidCidfromAddress(address);
+    // console.log(rid,cid);
+    let cell = document.querySelector(`.grid .col[rid="${rid}"][cid="${cid}"]`);
+    console.log(cell.innerText);
+    cell.style.textAlign="left";
+})
+
+rightBtn.addEventListener("click", function () {
+    let address = addressBar.value;
+    let { rid, cid } = getRidCidfromAddress(address);
+    console.log(rid, cid);
+    let cell = document.querySelector(`.col[rid="${rid}"][cid="${cid}"]`);
+    cell.style.textAlign = "right";
+})
+
+centerBtn.addEventListener("click", function () {
+    let address = addressBar.value;
+    let { rid, cid } = getRidCidfromAddress(address);
+    console.log(rid, cid);
+    let cell = document.querySelector(`.col[rid="${rid}"][cid="${cid}"]`);
+    cell.style.textAlign = "center";
+})
+
+fontBtn.addEventListener("change", function () {
+    let fontSize = fontBtn.value;
+    let address = addressBar.value;
+    let { rid, cid } = getRidCidfromAddress(address);
+    console.log(rid, cid);
+    let cell = document.querySelector(`.col[rid="${rid}"][cid="${cid}"]`);
+    console.log(fontSize);
+    cell.style.fontSize = fontSize+"px";
+})
+
+function getRidCidfromAddress(address){
+    //A1
+    let cellColAdr=address.charCodeAt(0);
+    let cellrowAdr=address.slice(1);
+    // console.log(cellColAdr);
+    let cid= cellColAdr-65;
+    let rid= Number(cellrowAdr)-1;
+    return {rid, cid};
+}
+
+Allcells[0].click();
