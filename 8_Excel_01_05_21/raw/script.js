1fa7b597b0982bb20eadd77982c30829793c9cc2
Arindam <arindamkeswani@gmail.com>
2021-05-07 22:54:36 +0530

    Added DOM questions

---
+++ 8_Excel_01_05_21/raw/script.js
@@ -0,0 +1,26 @@
+//Q1
+
+// let b = document.querySelectorAll(".ball");
+// let p= document.getElementsByTagName("p");
+// console.log(p[0].innerHTML);
+
+// p[0].innerHTML = "2 + 2 = 4";
+// for(let i=0;i<b.length;i++){
+//     b[i].style.backgroundColor = "blue";
+// }
+
+
+//Q3
+// let btn = document.querySelector("button");
+
+// let body=document.getElementsByTagName("body");
+
+// btn.addEventListener("click",function(){
+//     console.log("Reached");
+
+    
+//     let container = document.createElement("div");
+//     container.setAttribute("class","hello");
+//     container.innerHTML=`<div><p>Hello</p></div>`;
+//     body[0].appendChild(container);
+// })
