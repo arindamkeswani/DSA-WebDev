8fda1defcf9bd731200308cd241837b0642f5b4e
Arindam <arindamkeswani@gmail.com>
2021-06-09 00:02:09 +0530

    Updated DSA:Time and space

--- DSA/Time And Space/countSort.java
+++ DSA/Time And Space/countSort.java
@@ -1,28 +1,71 @@
+// 1. You are given an array(arr) of integers.
+// 2. You have to sort the given array in increasing order using count sort.
+// Input Format
+// An Integer n 
+// arr1
+// arr2..
+// n integers
+// Output Format
+// Check the sample ouput and question video.
 
-//O(n) =>[4n + (nRange)]
-//Not commonly preferred when range of values is very high
-//Space complexity = O(Math.max(n,nRange))
-int numUnique=max-min+1;
-int freq[] = new int[numUnique];
 
-for(int val:arr){ //map elements with their frequencies
-    freq[val-min]++;
-}
 
-for(int idx=1;idx<numUnique;i++){ //cumulative frequencies
-    freq[idx] += freq[idx-1];
-}
+import java.io.*;
+import java.util.*;
 
-int res[]=new int[arr.length]; //result array
+public class Main {
 
-for(int idx=arr.length-1;idx>=0;idx--){
-    int val = arr[idx]; //value of element from unsorted array
-    int pos = val-min; //position mapped in freq array
-    int place = freq[pos]; //place of current element in the resultant array acc to freq array
-    res[place-1] = val; //place - 1 is index of current element in resultant array
-    freq[pos]--; //update value of current elements in freq table
-}
+  public static void countSort(int[] arr, int min, int max) {
+   //write your code here
+   //O(n) =>[4n + (nRange)]
+    //Not commonly preferred when range of values is very high
+    //Space complexity = O(Math.max(n,nRange))
+    int numUnique=max-min+1;
+    int freq[] = new int[numUnique];
+    
+    for(int val:arr){ //map elements with their frequencies
+        freq[val-min]++;
+    }
+    
+    for(int idx=1;idx<numUnique;idx++){ //cumulative frequencies
+        freq[idx] += freq[idx-1];
+    }
+    
+    int res[]=new int[arr.length]; //result array
+    
+    for(int idx=arr.length-1;idx>=0;idx--){
+        int val = arr[idx]; //value of element from unsorted array
+        int pos = val-min; //position mapped in freq array
+        int place = freq[pos]; //place of current element in the resultant array acc to freq array
+        res[place-1] = val; //place - 1 is index of current element in resultant array
+        freq[pos]--; //update value of current elements in freq table
+    }
+    
+    for(int i=0;i<arr.length;i++){
+        arr[i] = res[i];
+    }
+   
+  }
 
-for(int i=0;i<arr.length;i++){
-    arr[i] = res[i];
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+      max = Math.max(max, arr[i]);
+      min = Math.min(min, arr[i]);
+    }
+    countSort(arr,min,max);
+    print(arr);
+  }
+
 }
--- DSA/Time And Space/radixSort.java
+++ DSA/Time And Space/radixSort.java
@@ -1,47 +1,75 @@
-//Works from least significant to most significant digits
-//sort full array based on least sig digit
-//Move towards left
+import java.io.*;
+import java.util.*;
 
-int max = Integer.MIN_VALUE;
-for(int val:arr){
-    if(max<val){
-        max=val;
+public class Main {
+
+  public static void radixSort(int[] arr) {
+    // write code here 
+    //Works from least significant to most significant digits
+    //sort full array based on least sig digit
+    //Move towards left
+    
+    int max = Integer.MIN_VALUE;
+    for(int val:arr){
+        if(max<val){
+            max=val;
+        }
     }
+    
+    int count=0;
+    while(max!=0){
+        count++;
+        max/=10;
+    }
+    
+    for(int i=1, exp=1 ;i<=count;i++, exp*=10){
+        countSort(arr,exp);
+    }
+  }
+
+  public static void countSort(int[] arr, int exp) {
+    // write code here
+    int freq[] = new int[10];
+    for(int vl : arr){
+        freq[(vl/exp)%10]++;
+    }
+    for(int i = 1 ; i < 10 ; i++){
+        freq[i] += freq[i-1];
+    }
+    
+    int res[] = new int[arr.length];
+    for(int i = arr.length-1 ; i >= 0 ; i--){
+        int vl = arr[i];
+        int pos = (vl/exp)%10;
+        int place = freq[pos];
+        res[place-1] = vl;
+        freq[pos]--;
+    }
+    
+    for(int i = 0 ; i < arr.length ; i++){
+        arr[i] = res[i];
+    }
+    System.out.print("After sorting on " + exp + " place -> ");
+    print(arr);
+    
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    radixSort(arr);
+    print(arr);
+  }
+
 }
-
-int count=0;
-while(max!=0){
-    count++;
-    max/=10;
-}
-
-for(int i=1, exp=1 ;i<=count;i++, exp*=10){
-    countSort(arr,exp);
-}
-
-//count sort function
-int freq[]= new int[10];
-
-for(int val:arr){
-    freq[(val/exp)*10]++;
-}
-
-for(int i=1;i<10;i++){
-    freq[i] = freq[i-1];
-}
-
-int res[] = new int[arr.length];
-for(int i=arr.length-1;i>0;i--){
-    int val = arr[i];
-    int pos = (val/exp)%10;
-    int place= freq[pos];
-    res[place-1] = val;
-    freq[pos]--;
-}
-
-for(int i=0;i<arr.length;i++){
-    arr[i] = res[i];
-}
-
-System.out.println("After sorting on " + exp+ " place->");
-print(arr);
