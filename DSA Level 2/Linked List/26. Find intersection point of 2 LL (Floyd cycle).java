e74be00dc37c5bb5101e9cec9d59e6369bd12538
Arindam <arindamkeswani@gmail.com>
2021-07-08 01:56:18 +0530

    Added hw questions in LevelUp: LL

--- DSA Level 2/Linked List/26. Find intersection point of 2 LL (Floyd cycle).java
+++ DSA Level 2/Linked List/26. Find intersection point of 2 LL (Floyd cycle).java
@@ -1,4 +1,20 @@
+1. Given the heads of two singly linked-lists headA and headB
+2. Return the node at which the two lists intersect. 
+3. If the two linked lists have no intersection, return null.
+Input Format
+Input is managed for you.
+Output Format
+Output is managed for you.
 
+Sample Input
+4
+14 12 8 7 
+2
+4
+7 2 6 5 
+Sample Output
+8
+__________________________________________________
 //floyd cycle algo
 go to tail node, store it
 connect it to either head
@@ -6,3 +22,89 @@ This forms a cycle
 then follow Q25 algo
 
 Change tail.next=null
+__________________________________________________
+import java.util.*;
+
+class Main {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode IntersectionNodeInTwoLL(ListNode headA, ListNode headB) {
+        ListNode curr=headA;
+        
+        while(curr.next!=null){
+            curr=curr.next;
+        }
+        curr.next= headA;
+        
+        ListNode fast=headB;
+        ListNode slow=headB;
+        
+        
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                
+                break;
+            }
+        }
+        
+        if(fast!=slow){
+            return null;
+        }
+        
+        //if code reaches here, that means we have established that cycle exists
+        fast=headB;
+        while(fast!=slow){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        
+        return slow;
+    }
+
+    // Input_code===================================================
+
+    public static ListNode makeList(int n) {
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head1 = makeList(scn.nextInt());
+
+        int idx = scn.nextInt();
+
+        ListNode head2 = makeList(scn.nextInt());
+
+        if (idx >= 0) {
+            ListNode curr = head1;
+            while (idx-- > 0)
+                curr = curr.next;
+
+            ListNode prev = head2;
+            while (prev.next != null)
+                prev = prev.next;
+
+            prev.next = curr;
+        }
+
+        ListNode ans = IntersectionNodeInTwoLL(head1, head2);
+        System.out.println(ans != null ? ans.val : -1);
+    }
+}
--- DSA Level 2/Linked List/27. Find intersection point of 2 LL (difference method).java
+++ DSA Level 2/Linked List/27. Find intersection point of 2 LL (difference method).java
@@ -1,4 +1,109 @@
+1. Given the heads of two singly linked-lists headA and headB
+2. Return the node at which the two lists intersect. 
+3. If the two linked lists have no intersection, return null.
+Input Format
+Input is managed for you.
+Output Format
+Output is managed for you.
+
+
+Sample Input
+4
+14 12 8 7 
+2
+4
+7 2 6 5 
+Sample Output
+8
+______________________________________________________
 //Level 1 algo
 Take out length of both LL
 Take diff and see which ll is smaller or larger
 traverse accordingly
+______________________________________________________
+import java.util.*;
+
+class Main {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+    
+    static int getLength(ListNode head){
+        ListNode curr=head;
+        int count=0;
+        while(curr!=null){
+            curr=curr.next;
+            count++;
+        }
+        return count;
+    }
+    public static ListNode IntersectionNodeInTwoLL(ListNode headA, ListNode headB) {
+        if(headA==null || headB==null){
+            return null;
+        }
+        
+        int aLen= getLength(headA);
+        int bLen= getLength(headB);
+        
+        int diff=Math.abs(aLen-bLen);
+        
+        
+        ListNode ptr1=aLen<bLen ? headA : headB;
+        ListNode ptr2=aLen<bLen ? headB : headA; //bigger list head, covers gap
+        
+        for(int i=0;i<diff;i++){
+            ptr2=ptr2.next;
+        }
+        
+        while(ptr2!=null && ptr1!=ptr2){
+            ptr1=ptr1.next;
+            ptr2=ptr2.next;
+        }
+        
+        
+        return ptr2;
+    }
+
+    // Input_code===================================================
+
+    public static ListNode makeList(int n) {
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head1 = makeList(scn.nextInt());
+
+        int idx = scn.nextInt();
+
+        ListNode head2 = makeList(scn.nextInt());
+
+        if (idx >= 0) {
+            ListNode curr = head1;
+            while (idx-- > 0)
+                curr = curr.next;
+
+            ListNode prev = head2;
+            while (prev.next != null)
+                prev = prev.next;
+
+            prev.next = curr;
+        }
+
+        ListNode ans = IntersectionNodeInTwoLL(head1, head2);
+        System.out.println(ans != null ? ans.val : -1);
+    }
+}
